6c6a51ca5eae3e321c2c2e93926388689d200011
Thomas William Tickle <ttickle@gmail.com>
2026-01-15 00:04:22 -0500

    LC 392: Is Subsequence

---
+++ src/easy/lc_392.rs
@@ -0,0 +1,35 @@
+/// LC 392: Is Subsequence
+
+struct Solution;
+
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let mut s_chars = s.chars().into_iter();
+        let mut s_char = s_chars.next();
+        
+        for t_char in t.chars() {
+            if s_char == Some(t_char) {
+                s_char = s_chars.next();
+            }
+        }
+        
+        s_char == None           
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::easy::lc_392::Solution;
+
+    #[test]
+    fn test_is_subsequence() {
+        let s = "abc".to_string();
+        let t = "ahbgdc".to_string();
+        assert_eq!(Solution::is_subsequence(s, t), true);
+
+        let s = "axc".to_string();
+        let t = "ahbgdc".to_string();
+        assert_eq!(Solution::is_subsequence(s, t), false);
+    }
+}
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -10,3 +10,4 @@ mod lc_238;
 mod lc_433;
 mod lc_334;
 mod lc_283;
+mod lc_392;
